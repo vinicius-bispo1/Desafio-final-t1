824614722c1aead51f65fdf1db0801705eddb932
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-12-02 09:33:50 -0300

    add responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,21 @@
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
+                    
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                    
+                </article>
+            </section>
         </section>
     )
 }
